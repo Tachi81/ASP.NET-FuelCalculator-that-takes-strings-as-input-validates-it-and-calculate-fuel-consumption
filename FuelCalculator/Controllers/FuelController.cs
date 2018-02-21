552e8c7eceabb2f960920a74deff9d8ed16d6555
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-21 15:14:44 +0100

    Added model Fuel with controller and "CalculateFuelCons" view. Uptaded front page to redirect to create view

---
+++ FuelCalculator/Controllers/FuelController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FuelCalculator.BusinessLogic;
+using FuelCalculator.Models;
+
+namespace FuelCalculator.Controllers
+{
+    public class FuelController : Controller
+    {
+       private FuelLogic _logic;
+        public FuelController()
+        {
+            _logic = new FuelLogic();
+        }
+        
+        // GET: Fuel
+        public ActionResult CalculateFuelCons()
+        {
+            return View("~/ViewS/Fuel/CalculateFuelCons.cshtml");
+        }
+
+        [HttpPost]
+        // POST: Fuel
+        public ActionResult CalculateFuelCons(Fuel model)
+        {
+            if (model.Distance==null || model.FuelUsed == null)
+            {
+                return View(model);
+            }
+            _logic.CalcAvgConsumption(model);
+            return View("Result", model);
+        }
+
+    }
+}
---
+++ FuelCalculator/Models/Fuel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuelCalculator.Models
+{
+    public class Fuel
+    {
+        public string Distance { get; set; }
+        public string FuelUsed { get; set; }
+        public string Result { get; set; }
+    }
+}
