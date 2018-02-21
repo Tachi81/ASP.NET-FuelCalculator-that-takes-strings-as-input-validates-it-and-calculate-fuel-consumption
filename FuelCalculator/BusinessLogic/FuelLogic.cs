0e3574708706ace7704079c00afb6d5dfd8607d4
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-21 15:17:34 +0100

    Added business logic and service tovalidate input,  Parse strings to int/floats, calculate the result and return is as result view

---
+++ FuelCalculator/BusinessLogic/FuelLogic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FuelCalculator.Models;
+
+namespace FuelCalculator.BusinessLogic
+{
+    public class FuelLogic
+    {
+        private ServiceFuel.ServiceFuel _service;
+
+        public FuelLogic()
+        {
+            _service = new ServiceFuel.ServiceFuel();
+        }
+
+        public void CalcAvgConsumption(Fuel model)
+        {
+            if (_service.StringToIntt(model.Distance) == 0)
+            {
+                model.Result = "Field Fuel can not be 0";
+            }
+            if (_service.StringToIntt(model.Distance) == -1 || _service.StringToIntt(model.FuelUsed) == -1)
+            {
+                model.Result = "Both fields must be fullfilled with numbers - not letters";
+            }
+            else
+            {
+                model.Result = $"Spalanie twojego auta to {_service.StringToFloat(model.FuelUsed) * 100 / _service.StringToIntt(model.Distance)} l/100km";
+
+            }
+
+        }
+    }
+}
---
+++ FuelCalculator/ServiceFuel/ServiceFuel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FuelCalculator.ServiceFuel
+{
+    public class ServiceFuel
+    {
+        public float StringToFloat(string text)
+        {
+            try
+            {
+                var floatted = float.Parse(text);
+                return floatted;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+        
+
+        public int StringToIntt(string text)
+        {
+            try
+            {
+                var inted = int.Parse(text);
+                return inted;
+            }
+            catch (Exception )
+            {
+                return -1;
+            }
+        }
+    }
+}
